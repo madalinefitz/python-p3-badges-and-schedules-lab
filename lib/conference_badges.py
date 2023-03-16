1f732bd4988c787b69cc22ab90c9dcf686b30684
madalinefitzpatrick <madaline.fitzpatrick@gmail.com>
2023-03-16 16:11:43 -0400

    lab completed

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,27 @@
+import ipdb
 def badge_maker(name):
-    return None
+    return(f"Hello, my name is {name}.")
 
 def batch_badge_creator(names):
-    return None
+    badges=[]
+    for name in names:
+        badges_string = (f"Hello, my name is {name}.")
+        badges.append(badges_string)
+    return badges
+        
 
 def assign_rooms(names):
-    return None
+    rooms=[]
+    for name in names:
+        rooms_message=(f"Hello, {name}! You'll be assigned to room {names.index(name) + 1}!")
+        rooms.append(rooms_message)
+    return rooms
+
+
 
 def printer(names):
-    return None
+    for message in batch_badge_creator(names):
+        print(message)
+    for message in assign_rooms(names):
+        print(message)
+    
